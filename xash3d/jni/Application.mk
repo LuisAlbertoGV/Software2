848a1be01eda40de4adbcd8702656a610f35c828
luisalbertov7 <luisalbertov7@hotmail.es>
2016-12-14 23:24:29 -0400

    Ultima version del Proyecto Xash3D

---
+++ xash3d/jni/Application.mk
@@ -0,0 +1,40 @@
+
+# Uncomment this if you're using STL in your project
+# See CPLUSPLUS-SUPPORT.html in the NDK documentation for more information
+#APP_STL := stlport_static 
+
+XASH_SDL ?= 1
+
+ifeq ($(XASH_SDL),1)
+APP_PLATFORM := android-12
+else
+APP_PLATFORM := android-8
+endif
+
+
+CFLAGS_OPT :=  -O3 -fomit-frame-pointer -ggdb -funsafe-math-optimizations -ftree-vectorize -fgraphite-identity -floop-interchange -funsafe-loop-optimizations -finline-limit=1024
+CFLAGS_OPT_ARM := -mthumb -mfpu=neon -mcpu=cortex-a9 -pipe -mvectorize-with-neon-quad -DVECTORIZE_SINCOS -fPIC
+CFLAGS_OPT_ARMv5 :=-march=armv6 -mfpu=vfp -marm -pipe
+CFLAGS_OPT_X86 := -mtune=atom -march=atom -mssse3 -mfpmath=sse -funroll-loops -pipe -DVECTORIZE_SINCOS
+CFLAGS_HARDFP := -D_NDK_MATH_NO_SOFTFP=1 -mhard-float -mfloat-abi=hard -DLOAD_HARDFP -DSOFTFP_LINK
+APPLICATIONMK_PATH = $(call my-dir)
+
+NANOGL_PATH := $(APPLICATIONMK_PATH)/src/NanoGL/nanogl
+
+XASH3D_PATH := $(APPLICATIONMK_PATH)/src/Xash3D/xash3d
+
+HLSDK_PATH  := $(APPLICATIONMK_PATH)/src/HLSDK/halflife/
+
+XASH3D_CONFIG := $(APPLICATIONMK_PATH)/xash3d_config.mk
+
+APP_ABI := x86 armeabi armeabi-v7a-hard
+# Use armeabi-v7a to disable hardfloat, armeabi to build armv5 xash3d
+# Change CFLAGS_OPT_ARMv5 to "-mfloat-abi=softfp -mfpu=vfp" and fix setup.mk of ndk to build armv6
+# Mods are built with both ABI support
+# ARMv6 and ARMv5 xash3d builds use softfp only and compatible only with softfp mods
+# Build both armeabi-v7a-hard and armeabi-v7a supported only for mods, not for engine
+
+APP_MODULES := xash menu client server NanoGL
+ifeq ($(XASH_SDL),1)
+APP_MODULES += SDL2
+endif
---
+++ xash3d/src/in/celest/xash3d/LauncherActivity.java
@@ -0,0 +1,143 @@
+package in.celest.xash3d;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.os.Build;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.content.Intent;
+import android.widget.EditText;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.Button;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.SharedPreferences;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnDismissListener;
+import android.net.Uri;
+import android.os.Environment;
+import java.lang.reflect.Method;
+import java.util.List;
+import java.io.File;
+
+import in.celest.xash3d.hl.R;
+
+public class LauncherActivity extends Activity {
+   // public final static String ARGV = "in.celest.xash3d.MESSAGE";
+	static EditText cmdArgs;
+	static CheckBox useVolume;
+	static EditText resPath;
+	static SharedPreferences mPref;
+	String getDefaultPath()
+	{
+		File dir = Environment.getExternalStorageDirectory();
+		if( dir != null && dir.exists() )
+			return dir.getPath() + "/xash";
+		return "/sdcard/xash";
+	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+        Button selectFolder = ( Button ) findViewById( R.id.button_select );
+		if ( Build.VERSION.SDK_INT < 21 )
+			selectFolder.setVisibility( View.GONE );
+		mPref = getSharedPreferences("engine", 0);
+		cmdArgs = (EditText)findViewById(R.id.cmdArgs);
+		cmdArgs.setText(mPref.getString("argv","-dev 3 -log"));
+		useVolume = ( CheckBox ) findViewById( R.id.useVolume );
+		useVolume.setChecked(mPref.getBoolean("usevolume",true));
+		resPath = ( EditText ) findViewById( R.id.cmdPath );
+		resPath.setText(mPref.getString("basedir", getDefaultPath()));
+	}
+    
+    public void startXash(View view)
+    {
+	Intent intent = new Intent(this, org.libsdl.app.SDLActivity.class);
+	intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+	SharedPreferences.Editor editor = mPref.edit();
+	editor.putString("argv", cmdArgs.getText().toString());
+	editor.putBoolean("usevolume",useVolume.isChecked());
+	editor.putString("basedir", resPath.getText().toString());
+	editor.commit();
+	editor.apply();
+	startActivity(intent);
+    }
+
+	public void aboutXash(View view)
+	{
+		final Activity a = this;
+		this.runOnUiThread(new Runnable() {
+			public void run()
+			{
+				final Dialog dialog = new Dialog(a);
+				dialog.setContentView(R.layout.about);
+				dialog.setCancelable(true);
+
+				dialog.show();
+			}
+		});
+	}
+
+	public void selectFolder(View view)
+	{
+		Intent intent = new Intent("android.intent.action.OPEN_DOCUMENT_TREE");
+		startActivityForResult(intent, 42);
+		resPath.setEnabled(false);
+	}
+
+	public void onActivityResult(int requestCode, int resultCode, Intent resultData) {
+		if (resultCode == RESULT_OK) {
+try{
+			final List<String> paths = resultData.getData().getPathSegments();
+			String[] parts = paths.get(1).split(":");
+			String storagepath = Environment.getExternalStorageDirectory().getPath() + "/";
+			String path = storagepath + parts[1];
+			if( path != null)
+				resPath.setText( path );
+			resPath.setEnabled(true);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		}
+		resPath.setEnabled(true);
+	}
+
+	public void createShortcut(View view)
+	{
+		Intent intent = new Intent(this, ShortcutActivity.class);
+		intent.putExtra( "basedir", resPath.getText().toString() );
+		intent.putExtra( "name", "Xash3D" );
+		intent.putExtra( "argv", cmdArgs.getText().toString() );
+		startActivity(intent);
+	}
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        //getMenuInflater().inflate(R.menu.menu_launcher, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        /*if (id == R.id.action_settings) {
+            return true;
+        }*/
+
+        return super.onOptionsItemSelected(item);
+    }
+}
